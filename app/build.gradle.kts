da3e745ab1da0b0667aefd083e77a131ff3e5b7f
Kostya Kilroy <kostyakilroy@gmail.com>
2023-10-08 14:01:42 +0500

    refactoring: dagger hilt added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     kotlin("android")
     kotlin("kapt")
+    id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
 }
 
@@ -49,12 +50,12 @@ dependencies {
     implementation ("androidx.recyclerview:recyclerview:1.3.1")
     implementation ("androidx.cardview:cardview:1.0.0")
 
-    //   Navigation components
+    //  Navigation components
     implementation ("androidx.fragment:fragment-ktx:1.6.1")
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.4")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.4")
 
-    //    Room database
+    //  Room database
     implementation ("androidx.room:room-runtime:2.5.2")
     implementation ("androidx.room:room-ktx:2.5.2")
     implementation ("androidx.legacy:legacy-support-v4:1.0.0")
@@ -62,10 +63,18 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     ksp ("androidx.room:room-compiler:2.5.2")
 
-    //    Coroutine
+    //  Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    //  Coroutine
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+kapt {
+    correctErrorTypes = true
 }
